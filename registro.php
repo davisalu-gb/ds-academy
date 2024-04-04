346cb4b406199eb8b973cc92c7f7ecfa51ff4528
davisalu-gb <162332995+davisalu-gb@users.noreply.github.com>
2024-04-04 12:35:32 -0300

    new

--- registro.php
+++ registro.php
@@ -16,28 +16,3 @@
         $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,senha,email,telefone) VALUES ('$nome','$senha','$email','$telefone')");
     }
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="registro.css">
-</head>
-<body>
-
-<div class="container">
-
-    <h2>Registro</h2>
-    <form action="registro.php" method="POST">
-        <input type="text" name="nome" id="nome" placeholder="Usuário" class="inputUser" required>
-        <input type="text" name="senha" id="senha" placeholder="Senha" class="inputUser" required>
-        <input type="text" name="email" id="email" placeholder="Email" class="inputUser" required>
-        <input type="telefone" name="telefone" id="telefone" placeholder="Telefone" class="inputUser" required>  
-        <input type="submit" name="submit" id="submit">Registrar-se</button>
-    </form>
-
-</div>
-
-</body>
-</html>
